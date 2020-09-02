75e81c19b2afdc29f22116eabc6fe0ce0f1ad44c
Roman Minyaylov <roman.minyaylov@gmail.com>
2020-09-02 17:50:59 +0300

    Update to SDK version 12.0.65

--- Video Capture SDK/WinForms/VB.Net/Simple Video Capture/My Project/AssemblyInfo.vb
+++ Video Capture SDK/WinForms/VB.Net/Simple Video Capture/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("12.0.63.0")>
-<Assembly: AssemblyFileVersion("12.0.63.0")>
+<Assembly: AssemblyVersion("12.0.65.0")>
+<Assembly: AssemblyFileVersion("12.0.65.0")>
