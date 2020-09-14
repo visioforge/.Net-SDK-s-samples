476ae06714f919b104f322ed502680b580542f20
Roman Minyaylov <roman.minyaylov@gmail.com>
2020-09-14 23:12:35 +0300

    Update to SDK version 12.0.73

--- Media Player SDK/WinForms/CSharp/Main Demo/Properties/AssemblyInfo.cs
+++ Media Player SDK/WinForms/CSharp/Main Demo/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("12.0.72.0")]
-[assembly: AssemblyFileVersion("12.0.72.0")]
+[assembly: AssemblyVersion("12.0.73.0")]
+[assembly: AssemblyFileVersion("12.0.73.0")]
