4005056ed1215bf1883c8963bb20681243385cf7
Roman Minyaylov <roman.minyaylov@gmail.com>
2020-10-23 13:56:23 +0300

    Update to SDK version 12.1.3

--- Video Capture SDK/WinForms/CSharp/Simple VideoCapture/Properties/AssemblyInfo.cs
+++ Video Capture SDK/WinForms/CSharp/Simple VideoCapture/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("12.1.2.0")]
-[assembly: AssemblyFileVersion("12.1.2.0")]
+[assembly: AssemblyVersion("12.1.3.0")]
+[assembly: AssemblyFileVersion("12.1.3.0")]
