273c1a85d2349a6caa9f5b5cd65790cc88f859f8
Roman Minyaylov <roman.minyaylov@gmail.com>
2020-10-27 14:10:51 +0300

    Update to SDK version 12.1.6

--- Video Capture SDK/WPF/CSharp/DV_Capture_NetCore/Properties/AssemblyInfo.cs
+++ Video Capture SDK/WPF/CSharp/DV_Capture_NetCore/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.1.3.0")]
-[assembly: AssemblyFileVersion("12.1.3.0")]
+[assembly: AssemblyVersion("12.1.6.0")]
+[assembly: AssemblyFileVersion("12.1.6.0")]
