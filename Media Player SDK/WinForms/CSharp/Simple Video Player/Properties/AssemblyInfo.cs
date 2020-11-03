395bf31b47d85438ea942b0bb242d51a296e0385
Roman Minyaylov <roman.minyaylov@gmail.com>
2020-11-03 17:04:15 +0300

    Update to SDK version 12.1.7

--- Media Player SDK/WinForms/CSharp/Simple Video Player/Properties/AssemblyInfo.cs
+++ Media Player SDK/WinForms/CSharp/Simple Video Player/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("12.1.6.0")]
-[assembly: AssemblyFileVersion("12.1.6.0")]
+[assembly: AssemblyVersion("12.1.7.0")]
+[assembly: AssemblyFileVersion("12.1.7.0")]
