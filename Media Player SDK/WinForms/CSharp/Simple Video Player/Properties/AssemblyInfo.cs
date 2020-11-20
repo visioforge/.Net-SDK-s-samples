eb7ccbec00a1173d22da3d519c3d4c46b228daa6
Roman Minyaylov <roman.minyaylov@gmail.com>
2020-11-20 16:47:57 +0300

    Update to SDK version 12.1.20

--- Media Player SDK/WinForms/CSharp/Simple Video Player/Properties/AssemblyInfo.cs
+++ Media Player SDK/WinForms/CSharp/Simple Video Player/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("12.1.19.0")]
-[assembly: AssemblyFileVersion("12.1.19.0")]
+[assembly: AssemblyVersion("12.1.20.0")]
+[assembly: AssemblyFileVersion("12.1.20.0")]
--- Video Capture SDK/WPF/CSharp/Main_Demo_NetCore/Properties/AssemblyInfo.cs
+++ Video Capture SDK/WPF/CSharp/Main_Demo_NetCore/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("12.1.19.0")]
-[assembly: AssemblyFileVersion("12.1.19.0")]
+[assembly: AssemblyVersion("12.1.20.0")]
+[assembly: AssemblyFileVersion("12.1.20.0")]
