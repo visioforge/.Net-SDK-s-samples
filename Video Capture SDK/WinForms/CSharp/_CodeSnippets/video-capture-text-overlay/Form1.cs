90fc696eb8b79c3441b67bf59ddad25366197bf6
Roman Minyaylov <roman.minyaylov@gmail.com>
2021-11-02 22:40:39 +0300

    Update to SDK version 15.0.2

---
+++ Video Capture SDK/WinForms/CSharp/_CodeSnippets/video-capture-text-overlay/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using VisioForge.Controls.VideoCapture;
+using VisioForge.Types;
+using VisioForge.Types.Output;
+using VisioForge.Types.VideoCapture;
+using VisioForge.Types.VideoEffects;
+
+namespace video_capture_text_overlay
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureCore videoCapture1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btStart_Click(object sender, EventArgs e)
+        {
+            videoCapture1.Video_CaptureDevice = new VideoCaptureSource(videoCapture1.Video_CaptureDevices[0].Name);
+            videoCapture1.Audio_CaptureDevice = new AudioCaptureSource(videoCapture1.Audio_CaptureDevices[3].Name);
+            videoCapture1.Mode = VideoCaptureMode.VideoCapture;
+            videoCapture1.Output_Filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "output.mp4");
+            videoCapture1.Output_Format = new MP4Output();
+
+            // add text overlay
+            videoCapture1.Video_Effects_Enabled = true;
+            videoCapture1.Video_Effects_Clear();
+            var textOverlay = new VideoEffectTextLogo(true) { Text = "Hello World!", Top = 50, Left = 50, FontColor = Color.Red };
+            videoCapture1.Video_Effects_Add(textOverlay);
+
+            await videoCapture1.StartAsync();
+        }
+
+        private async void btStop_Click(object sender, EventArgs e)
+        {
+            await videoCapture1.StopAsync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoCapture1 = new VideoCaptureCore(VideoView1 as IVideoView);
+        }
+    }
+}
