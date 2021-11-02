91a54cb457aa154aff25a2ccf2b13df4d8395f31
Roman Minyaylov <roman.minyaylov@gmail.com>
2021-11-02 18:09:06 +0300

    Update to SDK version 15.0.2

---
+++ Video Capture SDK/WinForms/CSharp/_CodeSnippets/ip-camera-preview/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using VisioForge.Controls.VideoCapture;
+using VisioForge.Types;
+using VisioForge.Types.Output;
+using VisioForge.Types.VideoCapture;
+
+namespace ip_camera_preview
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureCore videoCapture1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoCapture1 = new VideoCaptureCore(VideoView1 as IVideoView);
+        }
+
+        private async void btStart_Click(object sender, EventArgs e)
+        {
+            // Several engines are available. We'll use LAV as most compatible. For low latency RTSP playback, use the RTSP Low Latency engine.
+            videoCapture1.IP_Camera_Source = new IPCameraSourceSettings()
+            {
+                URL = "http://192.168.233.129:8000/camera/mjpeg",
+                Type = IPSourceEngine.Auto_LAV
+            };
+
+            videoCapture1.Audio_PlayAudio = videoCapture1.Audio_RecordAudio = false;
+            videoCapture1.Mode = VideoCaptureMode.IPPreview;
+
+            await videoCapture1.StartAsync();
+        }
+
+        private async void btStop_Click(object sender, EventArgs e)
+        {
+            await videoCapture1.StopAsync();
+        }
+    }
+}
