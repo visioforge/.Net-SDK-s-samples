03f11f8687bef61329297da276e0d731b927ef23
Roman Minyaylov <roman.minyaylov@gmail.com>
2021-11-03 11:34:54 +0300

    Update to SDK version 15.0.2

---
+++ Video Capture SDK/WinForms/CSharp/_CodeSnippets/video-capture-webcam-avi/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace video_capture_webcam_avi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VideoView1 = new VisioForge.Controls.UI.WinForms.VideoView();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // VideoView1
+            // 
+            this.VideoView1.BackColor = System.Drawing.Color.Black;
+            this.VideoView1.Location = new System.Drawing.Point(16, 14);
+            this.VideoView1.Name = "VideoView1";
+            this.VideoView1.Size = new System.Drawing.Size(640, 360);
+            this.VideoView1.StatusOverlay = null;
+            this.VideoView1.TabIndex = 0;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(16, 380);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(97, 380);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(75, 23);
+            this.btStop.TabIndex = 2;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 418);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.VideoView1);
+            this.Name = "Form1";
+            this.Text = "Video Capture SDK .Net - Video capture webcam to AVI code snippet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private VisioForge.Controls.UI.WinForms.VideoView VideoView1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+    }
+}
--- Video Capture SDK/WinForms/CSharp/_CodeSnippets/video-capture-webcam-avi/Form1.cs
+++ Video Capture SDK/WinForms/CSharp/_CodeSnippets/video-capture-webcam-avi/Form1.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+
 using VisioForge.Controls.VideoCapture;
 using VisioForge.Types;
 using VisioForge.Types.Output;
 using VisioForge.Types.VideoCapture;
 
-namespace video_capture_webcam_wmv
+namespace video_capture_webcam_avi
 {
     public partial class Form1 : Form
     {
@@ -17,15 +25,20 @@ namespace video_capture_webcam_wmv
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoCapture1 = new VideoCaptureCore(VideoView1 as IVideoView);
+        }
+
         private async void btStart_Click(object sender, EventArgs e)
         {
-            // default video and audio sources will be used
+            // Default video and audio sources will be used
             videoCapture1.Video_CaptureDevice = new VideoCaptureSource(videoCapture1.Video_CaptureDevices[0].Name);
             videoCapture1.Audio_CaptureDevice = new AudioCaptureSource(videoCapture1.Audio_CaptureDevices[0].Name);
-            videoCapture1.Output_Filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "output.wmv");
+            videoCapture1.Output_Filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "output.avi");
 
-            // WMV output with default settings
-            videoCapture1.Output_Format = new WMVOutput();
+            // Default AVI output with MJPEG for video and PCM for audio
+            videoCapture1.Output_Format = new AVIOutput();
             videoCapture1.Mode = VideoCaptureMode.VideoCapture;
 
             await videoCapture1.StartAsync();
@@ -34,11 +47,6 @@ namespace video_capture_webcam_wmv
         private async void btStop_Click(object sender, EventArgs e)
         {
             await videoCapture1.StopAsync();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            videoCapture1 = new VideoCaptureCore(VideoView1 as IVideoView);
         }
     }
 }
