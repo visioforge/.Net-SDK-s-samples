6db859fcc0992adeb10b6ceb2ea5456ce88d0009
Roman Minyaylov <roman.minyaylov@gmail.com>
2021-11-03 08:11:10 +0300

    Update to SDK version 15.0.2

---
+++ Video Capture SDK/WinForms/CSharp/_CodeSnippets/screen-capture-avi/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VisioForge.Controls.VideoCapture;
+using VisioForge.Types;
+using VisioForge.Types.Output;
+using VisioForge.Types.VideoCapture;
+
+namespace screen_capture_avi
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureCore videoCapture1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btStart_Click(object sender, EventArgs e)
+        {
+            videoCapture1.Screen_Capture_Source = new ScreenCaptureSourceSettings() { FullScreen = true };
+            videoCapture1.Audio_RecordAudio = videoCapture1.Audio_PlayAudio = false;
+            videoCapture1.Output_Filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "output.avi");
+
+            // Default AVI output with MJPEG for video and PCM for audio
+            videoCapture1.Output_Format = new AVIOutput(); 
+
+            videoCapture1.Mode = VideoCaptureMode.ScreenCapture;
+
+            await videoCapture1.StartAsync();
+        }
+
+        private async void btStop_Click(object sender, EventArgs e)
+        {
+            await videoCapture1.StopAsync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoCapture1 = new VideoCaptureCore(VideoView1 as IVideoView);
+        }
+    }
+}
