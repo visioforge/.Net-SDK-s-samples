1a600c2de11cb3c1364e3c93c1801a64693572d7
Roman Minyaylov <roman.minyaylov@gmail.com>
2021-11-05 16:27:32 +0300

    Update to SDK version 15.0.4

--- Video Capture SDK/Services/IP Capture/install_NET40.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil.exe "c:\Projects\_Projects\MediaFrameworkDotNet\_SOURCE\_DEMOS\Video Capture SDK\Service\IP Capture\bin\Debug\IPCaptureService.exe"
-pause
