f0f189f7ee346100682ba5ede3082a039f1cf155
Roman Miniailov <support@visioforge.com>
2022-07-04 14:16:21 +0200

    Update to SDK version 15.3.41

--- Media Player SDK/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
+++ Media Player SDK/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("15.3.40.0")]
-[assembly: AssemblyFileVersion("15.3.40.0")]
+[assembly: AssemblyVersion("15.3.41.0")]
+[assembly: AssemblyFileVersion("15.3.41.0")]
