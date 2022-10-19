b14fb96a383581142814a65cfbdcc0b75baadd90
Roman Miniailov <support@visioforge.com>
2022-10-19 20:08:03 +0200

    Update to SDK version 15.4.34

--- Media Player SDK X (crossplatform)/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
+++ Media Player SDK X (crossplatform)/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("15.4.33.0")]
-[assembly: AssemblyFileVersion("15.4.33.0")]
+[assembly: AssemblyVersion("15.4.34.0")]
+[assembly: AssemblyFileVersion("15.4.34.0")]
--- Media Player SDK/WinForms/VB .Net/DVD Player/My Project/AssemblyInfo.vb
+++ Media Player SDK/WinForms/VB .Net/DVD Player/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("15.4.33.0")>
-<Assembly: AssemblyFileVersion("15.4.33.0")>
+<Assembly: AssemblyVersion("15.4.34.0")>
+<Assembly: AssemblyFileVersion("15.4.34.0")>
