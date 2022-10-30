2772e3128f74d0723df05454710fbba8472a52a9
Roman Miniailov <support@visioforge.com>
2022-10-30 19:38:36 +0100

    Update to SDK version 15.4.42

--- Media Blocks SDK/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
+++ Media Blocks SDK/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("15.4.40.0")]
-[assembly: AssemblyFileVersion("15.4.40.0")]
+[assembly: AssemblyVersion("15.4.42.0")]
+[assembly: AssemblyFileVersion("15.4.42.0")]
