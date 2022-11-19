93396dca1fb1cc821ffbeaf78bbfd7eebdd35de6
Roman Miniailov <roman.minyaylov@gmail.com>
2022-11-19 01:03:47 +0100

    Update to SDK version 15.4.50

--- Video Capture SDK/WPF/CSharp/MultipleWebCameras/MainWindow.xaml.cs
+++ Video Capture SDK/WPF/CSharp/MultipleWebCameras/MainWindow.xaml.cs
@@ -77,6 +77,11 @@ namespace MultipleWebCameras
 
         private async void btStart1_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrEmpty(cbCamera1.Text))
+            {
+                return;
+            }
+
             VideoCapture1.Video_CaptureDevice = new VideoCaptureSource(cbCamera1.Text);
             VideoCapture1.Video_CaptureDevice.Format_UseBest = true;
 
@@ -92,7 +97,10 @@ namespace MultipleWebCameras
                 return;
             }
 
-            VideoCapture1.Video_CaptureDevice.FrameRate = videoFormat.FrameRates[videoFormat.FrameRates.Count - 1];
+            if (videoFormat.FrameRates.Count > 0)
+            {
+                VideoCapture1.Video_CaptureDevice.FrameRate = videoFormat.FrameRates[videoFormat.FrameRates.Count - 1];
+            }
 
             // VideoCapture1.OnError += VideoCapture1OnOnError;
             VideoCapture1.Mode = VideoCaptureMode.VideoPreview;
@@ -108,6 +116,11 @@ namespace MultipleWebCameras
 
         private async void btStart2_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrEmpty(cbCamera2.Text))
+            {
+                return;
+            }
+
             VideoCapture2.Video_CaptureDevice = new VideoCaptureSource(cbCamera2.Text);
             VideoCapture2.Video_CaptureDevice.Format_UseBest = true;
 
@@ -123,7 +136,10 @@ namespace MultipleWebCameras
                 return;
             }
 
-            VideoCapture2.Video_CaptureDevice.FrameRate = videoFormat.FrameRates[videoFormat.FrameRates.Count - 1];
+            if (videoFormat.FrameRates.Count > 0)
+            {
+                VideoCapture2.Video_CaptureDevice.FrameRate = videoFormat.FrameRates[videoFormat.FrameRates.Count - 1];
+            }
 
             // VideoCapture2.OnError += VideoCapture2OnOnError;
             VideoCapture2.Mode = VideoCaptureMode.VideoPreview;
