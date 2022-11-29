9b9c569694d4102c0704bca409f244c58b6b09b3
Roman Miniailov <roman.minyaylov@gmail.com>
2022-11-29 22:55:31 +0100

    Update to SDK version 15.4.57

--- Media Blocks SDK/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
+++ Media Blocks SDK/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("15.4.56.0")]
-[assembly: AssemblyFileVersion("15.4.56.0")]
+[assembly: AssemblyVersion("15.4.57.0")]
+[assembly: AssemblyFileVersion("15.4.57.0")]
--- Media Player SDK/WinForms/VB .Net/Two Windows/My Project/AssemblyInfo.vb
+++ Media Player SDK/WinForms/VB .Net/Two Windows/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("15.4.56.0")>
-<Assembly: AssemblyFileVersion("15.4.56.0")>
+<Assembly: AssemblyVersion("15.4.57.0")>
+<Assembly: AssemblyFileVersion("15.4.57.0")>
