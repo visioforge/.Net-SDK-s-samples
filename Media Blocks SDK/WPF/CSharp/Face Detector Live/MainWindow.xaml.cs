e1831be77570fce572fda9d23ca5b76d0e2ffeb1
Roman Miniailov <roman.minyaylov@gmail.com>
2023-02-11 19:55:25 +0100

    Update to SDK version 15.5.35

---
+++ Media Blocks SDK/WPF/CSharp/Face Detector Live/MainWindow.xaml.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VisioForge.Core.MediaBlocks;
+using VisioForge.Core.MediaBlocks.AudioRendering;
+using VisioForge.Core.MediaBlocks.Sources;
+using VisioForge.Core.MediaBlocks.VideoProcessing;
+using VisioForge.Core.MediaBlocks.VideoRendering;
+using VisioForge.Core.ONVIF;
+using VisioForge.Core.Types;
+using VisioForge.Core.Types.Events;
+using VisioForge.Core.Types.X.Sources;
+
+namespace Face_Detector_Live
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaBlocksPipeline _pipeline;
+
+        private VideoRendererBlock _videoRenderer;
+
+        private SystemVideoSourceBlock _videoSource;
+
+        private FaceDetectorBlock _detector;
+
+        private System.Timers.Timer _timer;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _pipeline = new MediaBlocksPipeline(true);
+            _pipeline.OnError += Pipeline_OnError;
+        }
+
+        private void Pipeline_OnError(object sender, ErrorsEventArgs e)
+        {
+            Dispatcher.Invoke((Action)(() =>
+            {
+                mmLog.Text = mmLog.Text + e.Message + Environment.NewLine;
+            }));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timer = new System.Timers.Timer(500);
+            _timer.Elapsed += _timer_Elapsed;
+
+            _pipeline = new MediaBlocksPipeline(true);
+            _pipeline.OnError += Pipeline_OnError;
+
+            Title += $" (SDK v{MediaBlocksPipeline.SDK_Version})";
+
+            var videoCaptureDevices = SystemVideoSourceBlock.GetDevices(_pipeline);
+            if (videoCaptureDevices.Length > 0)
+            {
+                foreach (var item in videoCaptureDevices)
+                {
+                    cbVideoInput.Items.Add(item.Name);
+                }
+
+                cbVideoInput.SelectedIndex = 0;
+            }
+        }
+
+        private async void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var position = await _pipeline.Position_GetAsync();
+
+            Dispatcher.Invoke(() =>
+            {
+                lbTime.Text = position.ToString("hh\\:mm\\:ss");
+            });
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            btStop_Click(null, null);
+
+            if (_pipeline != null)
+            {
+                _pipeline.OnError -= Pipeline_OnError;
+
+                _pipeline.Dispose();
+                _pipeline = null;
+            }
+        }
+
+        private void cbVideoInput_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbVideoInput.SelectedIndex != -1 && e != null && e.AddedItems.Count > 0)
+            {
+                var deviceName = (string)e.AddedItems[0];
+
+                if (!string.IsNullOrEmpty(deviceName))
+                {
+                    cbVideoFormat.Items.Clear();
+
+                    var device = SystemVideoSourceBlock.GetDevices(_pipeline).FirstOrDefault(x => x.Name == deviceName);
+                    if (device != null)
+                    {
+                        foreach (var item in device.VideoFormats)
+                        {
+                            cbVideoFormat.Items.Add(item.Name);
+                        }
+
+                        if (cbVideoFormat.Items.Count > 0)
+                        {
+                            cbVideoFormat.SelectedIndex = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void cbVideoFormat_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cbVideoFrameRate.Items.Clear();
+
+            if (cbVideoInput.SelectedIndex != -1 && e != null && e.AddedItems.Count > 0)
+            {
+                var deviceName = cbVideoInput.SelectedValue.ToString();
+                var format = (string)e.AddedItems[0];
+                if (!string.IsNullOrEmpty(deviceName) && !string.IsNullOrEmpty(format))
+                {
+                    var device = SystemVideoSourceBlock.GetDevices(_pipeline).FirstOrDefault(x => x.Name == deviceName);
+                    if (device != null)
+                    {
+                        var formatItem = device.VideoFormats.FirstOrDefault(x => x.Name == format);
+                        if (formatItem != null)
+                        {
+                            // build int range from tuple (min, max)    
+                            var frameRateList = formatItem.GetFrameRateRangeAsStringList();
+                            foreach (var item in frameRateList)
+                            {
+                                cbVideoFrameRate.Items.Add(item);
+                            }
+
+                            if (cbVideoFrameRate.Items.Count > 0)
+                            {
+                                cbVideoFrameRate.SelectedIndex = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void btStart_Click(object sender, RoutedEventArgs e)
+        {
+            mmLog.Clear();
+
+            if (cbVideoInput.SelectedIndex < 0)
+            {
+                MessageBox.Show("Select video input device");
+                return;
+            }
+
+            // video source
+            VideoCaptureDeviceSourceSettings videoSourceSettings = null;
+
+            var deviceName = cbVideoInput.Text;
+            var format = cbVideoFormat.Text;
+            if (!string.IsNullOrEmpty(deviceName) && !string.IsNullOrEmpty(format))
+            {
+                var device = SystemVideoSourceBlock.GetDevices(_pipeline).FirstOrDefault(x => x.Name == deviceName);
+                if (device != null)
+                {
+                    var formatItem = device.VideoFormats.FirstOrDefault(x => x.Name == format);
+                    if (formatItem != null)
+                    {
+                        videoSourceSettings = new VideoCaptureDeviceSourceSettings(device.Name)
+                        {
+                            Format = formatItem.ToFormat()
+                        };
+
+                        videoSourceSettings.Format.FrameRate = new VideoFrameRate(Convert.ToDouble(cbVideoFrameRate.Text));
+                    }
+                }
+            }
+
+            _videoSource = new SystemVideoSourceBlock(videoSourceSettings);
+
+            // video renderer
+            _videoRenderer = new VideoRendererBlock(_pipeline, VideoView1);
+
+            // detector
+            _detector = new FaceDetectorBlock();
+
+            // connect all
+            _pipeline.Connect(_videoSource.Output, _detector.Input);
+            _pipeline.Connect(_detector.Output, _videoRenderer.Input);
+
+            // start
+            await _pipeline.StartAsync();
+
+            _timer.Start();
+        }
+
+        private async void btStop_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+
+            await _pipeline?.StopAsync();
+
+            _pipeline?.ClearBlocks();
+
+            VideoView1.CallRefresh();
+        }
+    }
+}
---
+++ Media Blocks SDK/WPF/CSharp/Face Detector Live/readme.md
@@ -0,0 +1,20 @@
+# Media Blocks SDK .Net - Face Detector Live Demo (WPF)
+
+Face Detector Live Demo is an application that uses the Media Blocks SDK .Net to detect faces in live video.
+
+## Features
+
+- Face detection
+- Video preview
+
+## Supported frameworks
+
+- .Net 4.7.2
+- .Net Core 3.1
+- .Net 5
+- .Net 6
+- .Net 7
+
+---
+
+[Media Blocks SDK .Net product page](https://www.visioforge.com/media-blocks-sdk)
