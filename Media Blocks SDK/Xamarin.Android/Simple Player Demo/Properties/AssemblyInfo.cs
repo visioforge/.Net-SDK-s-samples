9b5bd305697a729b5ed45474d213a26f996f886a
Roman Miniailov <roman.minyaylov@gmail.com>
2023-03-18 23:48:20 +0100

    Update to SDK version 15.5.49

--- Media Blocks SDK/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
+++ Media Blocks SDK/Xamarin.Android/Simple Player Demo/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("15.5.48.0")]
-[assembly: AssemblyFileVersion("15.5.48.0")]
+[assembly: AssemblyVersion("15.5.49.0")]
+[assembly: AssemblyFileVersion("15.5.49.0")]
