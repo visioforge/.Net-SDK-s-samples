f2f56d1508045cd56b9b96943795dd2f9144218f
Roman Miniailov <roman.minyaylov@gmail.com>
2023-03-30 00:07:04 +0200

    Update to SDK version 15.5.55

--- Media Player SDK/WinForms/VB .Net/Video Player/My Project/AssemblyInfo.vb
+++ Media Player SDK/WinForms/VB .Net/Video Player/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("15.5.54.0")>
-<Assembly: AssemblyFileVersion("15.5.54.0")>
+<Assembly: AssemblyVersion("15.5.55.0")>
+<Assembly: AssemblyFileVersion("15.5.55.0")>
