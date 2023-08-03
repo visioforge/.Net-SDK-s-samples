c8fae89ef537415b2da4acb8a82486b54e750906
Roman Miniailov <roman.minyaylov@gmail.com>
2023-08-03 19:35:43 +0200

    Update to SDK version 15.5.94

---
+++ Video Capture SDK X (crossplatform)/MAUI/QRReader/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QRReader
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Video Capture SDK X (crossplatform)/MAUI/QRReader/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace QRReader
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
