367c5498a0009b4f6fc442e1798656a65d4a5077
Roman Miniailov <roman.minyaylov@gmail.com>
2023-11-22 00:30:54 +0100

    Update to SDK version 15.7.11

--- Media Player SDK/WinForms/VB .Net/Audio Player/My Project/AssemblyInfo.vb
+++ Media Player SDK/WinForms/VB .Net/Audio Player/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("15.7.10.0")>
-<Assembly: AssemblyFileVersion("15.7.10.0")>
+<Assembly: AssemblyVersion("15.7.11.0")>
+<Assembly: AssemblyFileVersion("15.7.11.0")>
