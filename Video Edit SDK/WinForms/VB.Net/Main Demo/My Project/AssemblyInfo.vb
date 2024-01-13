feda2d1219f1c014e24d5d7eb3269eeb8d55da2c
Roman Miniailov <roman.minyaylov@gmail.com>
2024-01-13 20:12:55 +0100

    Update to SDK version 15.7.40

--- Video Edit SDK/WinForms/VB.Net/Main Demo/My Project/AssemblyInfo.vb
+++ Video Edit SDK/WinForms/VB.Net/Main Demo/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("15.7.39.0")>
-<Assembly: AssemblyFileVersion("15.7.39.0")>
+<Assembly: AssemblyVersion("15.7.40.0")>
+<Assembly: AssemblyFileVersion("15.7.40.0")>
