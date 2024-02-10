0894cb8a6491d193cacd6bee65a6514f6d85105a
Roman Miniailov <roman.minyaylov@gmail.com>
2024-02-10 18:26:35 +0100

    Update to SDK version 15.8.16

--- Video Edit SDK/Console/CSharp/Main Demo CLI/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "<Pending>", Scope = "type", Target = "~T:VE_Main_Demo_CLI.Program")]
--- Video Edit SDK/WPF/CSharp/Main Demo/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Major Code Smell", "S1871:Two branches in a conditional structure should not have exactly the same implementation", Justification = "<Pending>", Scope = "member", Target = "~M:Main_Demo.Window1.BtOutputConfigure_Click(System.Object,System.Windows.RoutedEventArgs)")]
