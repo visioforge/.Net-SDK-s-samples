62bdc12ba45161f03565503b04e40890e7ea7215
Roman Miniailov <roman.minyaylov@gmail.com>
2024-07-27 14:40:37 +0200

    Update to SDK version 15.9.70

--- Video Capture SDK/WinForms/VB.Net/Main Demo/My Project/AssemblyInfo.vb
+++ Video Capture SDK/WinForms/VB.Net/Main Demo/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("15.9.69.0")>
-<Assembly: AssemblyFileVersion("15.9.69.0")>
+<Assembly: AssemblyVersion("15.9.70.0")>
+<Assembly: AssemblyFileVersion("15.9.70.0")>
