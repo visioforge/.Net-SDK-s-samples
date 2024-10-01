eb33a235b23e5c1762c632f6c3599257a7eb2372
Roman Miniailov <roman.minyaylov@gmail.com>
2024-10-01 08:44:41 +0200

    Update to SDK version 15.10.17

---
+++ Media Blocks SDK/_CodeSnippets/memory-player/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Windows.Forms;
+
+using VisioForge.Core.Types.VideoCapture;
+using VisioForge.Core;
+using VisioForge.Core.VideoCaptureX;
+using VisioForge.Core.Types.X.AudioRenderers;
+using VisioForge.Core.Types.X.Output;
+using VisioForge.Core.Types.X.Sources;
+using System.Linq;
+using VisioForge.Core.MediaBlocks;
+using VisioForge.Core.MediaBlocks.VideoRendering;
+using VisioForge.Core.MediaBlocks.Sources;
+using VisioForge.Core.MediaBlocks.AudioRendering;
+using Gst.App;
+using VisioForge.Core.MediaBlocks.Parsers;
+using VisioForge.Core.Types.X.VideoEncoders;
+using System.Threading;
+using System.Timers;
+using System.IO;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using VisioForge.Core.MediaBlocks.Special;
+
+namespace media_player
+{
+    public partial class Form1 : Form
+    {
+        private MediaBlocksPipeline _pipeline;
+
+        private PushSourceBlock _source;
+
+        private DecodeBinBlock _decoder;
+
+        private VideoRendererBlock _videoRenderer;
+
+        private Thread _pushThread;
+
+        private System.Timers.Timer _tmPosition;
+
+        private const int CHUNK_SIZE = 4096;
+
+        private volatile bool _stopFlag;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _tmPosition = new System.Timers.Timer(1000);
+            _tmPosition.Elapsed += _tmPosition_Elapsed;
+        }
+
+        private void _tmPosition_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(async () =>
+            {
+                if (_pipeline != null)
+                {
+                    var pos = await _pipeline.Position_GetAsync();
+                    lbTimestamp.Text = pos.ToString(@"hh\:mm\:ss");
+                }
+            }));
+        }
+
+        private async void btStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(edFilename.Text))
+            {
+                MessageBox.Show("Please select an H264 file.");
+                return;
+            }
+
+            var filename = edFilename.Text;
+            var httpSource = filename.Contains("http://") || filename.Contains("https://");
+
+            // Create MediaBlocks pipeline
+            _pipeline = new MediaBlocksPipeline();
+            _pipeline.OnStop += (sender2, args) =>
+            {
+                _stopFlag = true;
+            };
+
+            // Add source
+            var sourceSettings = new PushDataSourceSettings();
+           // var caps = new Gst.Caps("video/x-h264");
+           // caps.SetValue("stream-format", new GLib.Value("byte-stream"));
+           // sourceSettings.Caps = caps;
+            sourceSettings.PushFormat = PushSourceFormat.Bytes;
+            sourceSettings.StreamType = PushSourceStreamType.Stream;
+            sourceSettings.IsLive = true;
+            sourceSettings.PadMediaType = MediaBlockPadMediaType.Video;
+            sourceSettings.BlockPushData = true;
+
+            _source = new PushSourceBlock(sourceSettings);
+
+            // Add decoder and parser
+            _decoder = new DecodeBinBlock(renderAudio: false);
+
+            // Add video renderer
+            _videoRenderer = new VideoRendererBlock(_pipeline, VideoView1) { IsSync = true };
+
+            // Connect blocks
+            _pipeline.Connect(_source.Output, _decoder.Input);
+            _pipeline.Connect(_decoder.VideoOutput, _videoRenderer.Input);
+
+            _stopFlag = false;
+
+            // Create pipeline and configure elements
+            await _pipeline.StartAsync(onlyPreload: true);
+
+            // Push data loop
+            _pushThread = new Thread(async () =>
+            {
+                Stream stream = null;
+                HttpClient httpClient = null;
+                HttpResponseMessage response = null;
+
+                if (httpSource)
+                {
+                    httpClient = new HttpClient();
+                    response = await httpClient.GetAsync(edFilename.Text, HttpCompletionOption.ResponseHeadersRead);
+                    response.EnsureSuccessStatusCode();
+
+                    stream = await response.Content.ReadAsStreamAsync();
+                }
+                else
+                {
+                    stream = System.IO.File.OpenRead(edFilename.Text);
+                }
+
+                var buffer = new byte[CHUNK_SIZE];
+                int len = 0;
+                while ((len = stream.Read(buffer, 0, CHUNK_SIZE)) > 0 && !_stopFlag)
+                {
+                    _source.PushData(buffer, len);
+                }
+
+                stream.Close();
+
+                response?.Dispose();
+                httpClient?.Dispose();
+
+                _source.SendEOS();
+            });
+
+            _pushThread.Start();
+
+            // Start playback
+            await _pipeline.ResumeAsync();
+
+            _tmPosition.Start();
+        }
+
+        private async void btStop_Click(object sender, EventArgs e)
+        {
+            _stopFlag = true;
+
+            _tmPosition.Stop();
+
+            await _pipeline.StopAsync();
+
+            await _pipeline.DisposeAsync();
+        }
+
+        private async Task<Stream> OpenHTTPStreamAsync()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            using (HttpResponseMessage response = await httpClient.GetAsync(edFilename.Text, HttpCompletionOption.ResponseHeadersRead))
+            {
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsStreamAsync();
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await VisioForgeX.InitSDKAsync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            VisioForgeX.DestroySDK();
+        }
+
+        private void btOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                edFilename.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
