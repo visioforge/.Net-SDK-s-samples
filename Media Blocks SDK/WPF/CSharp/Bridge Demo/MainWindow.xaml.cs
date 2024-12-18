2e2f9f65e2a5e5486f2924b908c5cb4e711d09fb
Roman Miniailov <support@visioforge.com>
2024-12-18 17:09:35 +0100

    Update to SDK version 2025.0.16

--- Media Blocks SDK/WPF/CSharp/Bridge Demo/MainWindow.xaml.cs
+++ Media Blocks SDK/WPF/CSharp/Bridge Demo/MainWindow.xaml.cs
@@ -72,7 +72,7 @@ namespace Bridge_Demo
             Title = Title.Replace("[FIRST TIME LOAD, BUILDING THE REGISTRY...]", "");
 
             Title += $" (SDK v{MediaBlocksPipeline.SDK_Version})";
-            edFilename.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "output.mp4");
+            edFilename.Text = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "output.mp4");
 
             await CreateSourceEngineAsync();
 
