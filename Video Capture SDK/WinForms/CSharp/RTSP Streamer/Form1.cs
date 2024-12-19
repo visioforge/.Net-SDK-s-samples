35dedc8e49f78c85634eaf93446c06c3fa5a29a2
Roman Miniailov <support@visioforge.com>
2024-12-19 13:37:15 +0100

    added RTSP streamer sample

---
+++ Video Capture SDK/WinForms/CSharp/RTSP Streamer/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VisioForge.Core.Types;
+using VisioForge.Core.Types.Events;
+using VisioForge.Core.Types.FFMPEGEXE;
+using VisioForge.Core.Types.Output;
+using VisioForge.Core.Types.VideoCapture;
+using VisioForge.Core.Types.X.AudioEncoders;
+using VisioForge.Core.Types.X.VideoEncoders;
+using VisioForge.Core.UI;
+using VisioForge.Core.VideoCapture;
+using VisioForge.Core.Windows.Types.Output;
+
+namespace RTSP_Streamer
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer tmRecording = new System.Timers.Timer(1000);
+
+        private VideoCaptureCore VideoCapture1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await CreateEngineAsync();
+
+            Text += $" (SDK v{VideoCapture1.SDK_Version()})";
+
+            tmRecording.Elapsed += (senderx, args) =>
+            {
+                UpdateRecordingTime();
+            };
+
+            foreach (var device in VideoCapture1.Video_CaptureDevices())
+            {
+                cbVideoInputDevice.Items.Add(device.Name);
+            }
+
+            if (cbVideoInputDevice.Items.Count > 0)
+            {
+                cbVideoInputDevice.SelectedIndex = 0;
+            }
+
+            cbVideoInputDevice_SelectedIndexChanged(null, null);
+
+            foreach (var device in VideoCapture1.Audio_CaptureDevices())
+            {
+                cbAudioInputDevice.Items.Add(device.Name);
+            }
+
+            if (cbAudioInputDevice.Items.Count > 0)
+            {
+                cbAudioInputDevice.SelectedIndex = 0;
+                cbAudioInputDevice_SelectedIndexChanged(null, null);
+            }
+
+            cbAudioInputLine.Items.Clear();
+
+            if (!string.IsNullOrEmpty(cbAudioInputDevice.Text))
+            {
+                var deviceItem =
+                    VideoCapture1.Audio_CaptureDevices().FirstOrDefault(device => device.Name == cbAudioInputDevice.Text);
+                if (deviceItem != null)
+                {
+                    foreach (string line in deviceItem.Lines)
+                    {
+                        cbAudioInputLine.Items.Add(line);
+                    }
+
+                    if (cbAudioInputLine.Items.Count > 0)
+                    {
+                        cbAudioInputLine.SelectedIndex = 0;
+                    }
+                }
+            }
+
+            VideoCapture1.Video_Renderer_SetAuto();
+        }
+
+        private async Task CreateEngineAsync()
+        {
+            VideoCapture1 = await VideoCaptureCore.CreateAsync(VideoView1 as IVideoView);
+
+            VideoCapture1.OnError += VideoCapture1_OnError;
+        }
+
+        private void DestroyEngine()
+        {
+            if (VideoCapture1 != null)
+            {
+                VideoCapture1.OnError -= VideoCapture1_OnError;
+
+                VideoCapture1.Dispose();
+                VideoCapture1 = null;
+            }
+        }
+
+        private void cbAudioInputDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbAudioInputDevice.SelectedIndex != -1)
+            {
+                cbAudioInputFormat.Items.Clear();
+
+                var deviceItem = VideoCapture1.Audio_CaptureDevices().FirstOrDefault(device => device.Name == cbAudioInputDevice.Text);
+                if (deviceItem == null)
+                {
+                    return;
+                }
+
+                var defaultValue = "PCM, 44100 Hz, 16 Bits, 2 Channels";
+                var defaultValueExists = false;
+                foreach (string format in deviceItem.Formats)
+                {
+                    cbAudioInputFormat.Items.Add(format);
+
+                    if (defaultValue == format)
+                    {
+                        defaultValueExists = true;
+                    }
+                }
+
+                if (cbAudioInputFormat.Items.Count > 0)
+                {
+                    cbAudioInputFormat.SelectedIndex = 0;
+
+                    if (defaultValueExists)
+                    {
+                        cbAudioInputFormat.Text = defaultValue;
+                    }
+                }
+
+                cbAudioInputLine.Items.Clear();
+
+                foreach (string line in deviceItem.Lines)
+                {
+                    cbAudioInputLine.Items.Add(line);
+                }
+
+                if (cbAudioInputLine.Items.Count > 0)
+                {
+                    cbAudioInputLine.SelectedIndex = 0;
+                }
+
+                btAudioInputDeviceSettings.Enabled = deviceItem.DialogDefault;
+            }
+        }
+
+        private void btAudioInputDeviceSettings_Click(object sender, EventArgs e)
+        {
+            VideoCapture1.Audio_CaptureDevice_SettingsDialog_Show(IntPtr.Zero, cbAudioInputDevice.Text);
+        }
+
+        private void cbVideoInputDevice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbVideoInputDevice.SelectedIndex != -1)
+            {
+                cbVideoInputFormat.Items.Clear();
+                var deviceItem = VideoCapture1.Video_CaptureDevices().FirstOrDefault(device => device.Name == cbVideoInputDevice.Text);
+                if (deviceItem == null)
+                {
+                    return;
+                }
+
+                foreach (var format in deviceItem.VideoFormats)
+                {
+                    cbVideoInputFormat.Items.Add(format.Name);
+                }
+
+                if (cbVideoInputFormat.Items.Count > 0)
+                {
+                    cbVideoInputFormat.SelectedIndex = 0;
+                    cbVideoInputFormat_SelectedIndexChanged(null, null);
+                }
+
+                btVideoCaptureDeviceSettings.Enabled = deviceItem.DialogDefault;
+            }
+        }
+
+        private void cbVideoInputFormat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(cbVideoInputFormat.Text))
+            {
+                return;
+            }
+
+            if (cbVideoInputDevice.SelectedIndex != -1)
+            {
+                var deviceItem = VideoCapture1.Video_CaptureDevices().FirstOrDefault(device => device.Name == cbVideoInputDevice.Text);
+                if (deviceItem == null)
+                {
+                    return;
+                }
+
+                var videoFormat = deviceItem.VideoFormats.FirstOrDefault(format => format.Name == cbVideoInputFormat.Text);
+                if (videoFormat == null)
+                {
+                    return;
+                }
+
+                cbVideoInputFrameRate.Items.Clear();
+                foreach (var frameRate in videoFormat.FrameRates)
+                {
+                    cbVideoInputFrameRate.Items.Add(frameRate.ToString(CultureInfo.CurrentCulture));
+                }
+
+                if (cbVideoInputFrameRate.Items.Count > 0)
+                {
+                    cbVideoInputFrameRate.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void btVideoCaptureDeviceSettings_Click(object sender, EventArgs e)
+        {
+            VideoCapture1.Video_CaptureDevice_SettingsDialog_Show(IntPtr.Zero, cbVideoInputDevice.Text);
+        }
+
+        private void Log(string txt)
+        {
+            if (IsHandleCreated)
+            {
+                Invoke((Action)(() => { mmLog.Text = mmLog.Text + txt + Environment.NewLine; }));
+            }
+        }
+
+        private void VideoCapture1_OnError(object sender, ErrorsEventArgs e)
+        {
+            Log(e.Message);
+        }
+
+        private async void btStart_Click(object sender, EventArgs e)
+        {
+            mmLog.Clear();
+
+            VideoCapture1.Video_Sample_Grabber_Enabled = true;
+
+            VideoCapture1.Debug_Mode = cbDebugMode.Checked;
+            VideoCapture1.Debug_Dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VisioForge");
+
+            VideoCapture1.Audio_RecordAudio = false;
+            VideoCapture1.Audio_PlayAudio = false;
+
+            // apply capture parameters
+            VideoCapture1.Video_CaptureDevice = new VideoCaptureSource(cbVideoInputDevice.Text);
+            VideoCapture1.Video_CaptureDevice.Format_UseBest = false;
+            VideoCapture1.Video_CaptureDevice.Format = cbVideoInputFormat.Text;
+
+            if (cbVideoInputFrameRate.SelectedIndex != -1)
+            {
+                VideoCapture1.Video_CaptureDevice.FrameRate = new VideoFrameRate(Convert.ToDouble(cbVideoInputFrameRate.Text));
+            }
+
+            //VideoCapture1.Audio_CaptureDevice = new AudioCaptureSource(cbAudioInputDevice.Text);
+            //VideoCapture1.Audio_CaptureDevice.Format_UseBest = false;
+            //VideoCapture1.Audio_CaptureDevice.Format = cbAudioInputFormat.Text;
+
+            // network streaming
+            VideoCapture1.Network_Streaming_Enabled = true;
+            VideoCapture1.Network_Streaming_Audio_Enabled = false;
+
+            VideoCapture1.Network_Streaming_Format = NetworkStreamingFormat.RTSP_X;
+
+            VideoCapture1.Network_Streaming_URL = "rtsp://127.0.0.1:7777/test";
+            VideoCapture1.Network_Streaming_Output = new RTSPXOutput(
+                new OpenH264EncoderSettings(VideoQuality.High),
+                new VOAACEncoderSettings());
+
+            VideoCapture1.Network_Streaming_URL = edNetworkUDPURL.Text;
+
+            // start
+            await VideoCapture1.StartAsync();
+
+            tcMain.SelectedIndex = 2;
+            tmRecording.Start();
+        }
+
+        private async void btStop_Click(object sender, EventArgs e)
+        {
+            tmRecording.Stop();
+
+            await VideoCapture1.StopAsync();
+        }
+
+        private void llVideoTutorials_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var startInfo = new ProcessStartInfo("explorer.exe", HelpLinks.VideoTutorials);
+            Process.Start(startInfo);
+        }
+
+        private async void btResume_Click(object sender, EventArgs e)
+        {
+            await VideoCapture1.ResumeAsync();
+        }
+
+        private async void btPause_Click(object sender, EventArgs e)
+        {
+            await VideoCapture1.PauseAsync();
+        }
+
+        private void UpdateRecordingTime()
+        {
+            if (IsHandleCreated)
+            {
+                var ts = VideoCapture1.Duration_Time();
+
+                if (Math.Abs(ts.TotalMilliseconds) < 0.01)
+                {
+                    return;
+                }
+
+                BeginInvoke(
+                    (Action)(() =>
+                    {
+                        lbTimestamp.Text = "Recording time: " + ts.ToString(@"hh\:mm\:ss");
+                    }));
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DestroyEngine();
+        }
+    }
+}
