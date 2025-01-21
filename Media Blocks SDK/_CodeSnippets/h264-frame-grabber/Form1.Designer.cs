7b0ff92b9a5f4f095a803e24a86fe674deb4ec0b
Roman Miniailov <support@visioforge.com>
2025-01-21 16:51:46 +0100

    Update to SDK version 2025.1.10

---
+++ Media Blocks SDK/_CodeSnippets/h264-frame-grabber/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace media_player
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btStart = new System.Windows.Forms.Button();
+            btStop = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            edFilename = new System.Windows.Forms.TextBox();
+            btOpenFile = new System.Windows.Forms.Button();
+            openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            lbTimestamp = new System.Windows.Forms.Label();
+            lbStatus = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // btStart
+            // 
+            btStart.Location = new System.Drawing.Point(27, 81);
+            btStart.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            btStart.Name = "btStart";
+            btStart.Size = new System.Drawing.Size(126, 43);
+            btStart.TabIndex = 1;
+            btStart.Text = "Start";
+            btStart.UseVisualStyleBackColor = true;
+            btStart.Click += btStart_Click;
+            // 
+            // btStop
+            // 
+            btStop.Location = new System.Drawing.Point(164, 81);
+            btStop.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            btStop.Name = "btStop";
+            btStop.Size = new System.Drawing.Size(126, 43);
+            btStop.TabIndex = 2;
+            btStop.Text = "Stop";
+            btStop.UseVisualStyleBackColor = true;
+            btStop.Click += btStop_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(27, 20);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(191, 25);
+            label1.TabIndex = 3;
+            label1.Text = "File name or HTTP URL";
+            // 
+            // edFilename
+            // 
+            edFilename.Location = new System.Drawing.Point(214, 15);
+            edFilename.Name = "edFilename";
+            edFilename.Size = new System.Drawing.Size(837, 31);
+            edFilename.TabIndex = 4;
+            edFilename.Text = "C:\\Samples\\!video.mp4";
+            // 
+            // btOpenFile
+            // 
+            btOpenFile.Location = new System.Drawing.Point(1057, 15);
+            btOpenFile.Name = "btOpenFile";
+            btOpenFile.Size = new System.Drawing.Size(37, 33);
+            btOpenFile.TabIndex = 5;
+            btOpenFile.Text = "...";
+            btOpenFile.UseVisualStyleBackColor = true;
+            btOpenFile.Click += btOpenFile_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lbTimestamp
+            // 
+            lbTimestamp.AutoSize = true;
+            lbTimestamp.Location = new System.Drawing.Point(1027, 91);
+            lbTimestamp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lbTimestamp.Name = "lbTimestamp";
+            lbTimestamp.Size = new System.Drawing.Size(80, 25);
+            lbTimestamp.TabIndex = 7;
+            lbTimestamp.Text = "00:00:00";
+            // 
+            // lbStatus
+            // 
+            lbStatus.AutoSize = true;
+            lbStatus.Location = new System.Drawing.Point(311, 91);
+            lbStatus.Name = "lbStatus";
+            lbStatus.Size = new System.Drawing.Size(159, 25);
+            lbStatus.TabIndex = 8;
+            lbStatus.Text = "Received frames: 0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1123, 168);
+            Controls.Add(lbStatus);
+            Controls.Add(lbTimestamp);
+            Controls.Add(btOpenFile);
+            Controls.Add(edFilename);
+            Controls.Add(label1);
+            Controls.Add(btStop);
+            Controls.Add(btStart);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            Name = "Form1";
+            Text = "Media Blocks SDK .Net - H264 frame grabber code snippet";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox edFilename;
+        private System.Windows.Forms.Button btOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lbTimestamp;
+        private System.Windows.Forms.Label lbStatus;
+    }
+}
---
+++ Media Blocks SDK/_CodeSnippets/h264-frame-grabber/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows.Forms;
+
+using VisioForge.Core.Types.VideoCapture;
+using VisioForge.Core;
+using VisioForge.Core.VideoCaptureX;
+using VisioForge.Core.Types.X.AudioRenderers;
+using VisioForge.Core.Types.X.Output;
+using VisioForge.Core.Types.X.Sources;
+using System.Linq;
+using VisioForge.Core.MediaBlocks;
+using VisioForge.Core.MediaBlocks.VideoRendering;
+using VisioForge.Core.MediaBlocks.Sources;
+using VisioForge.Core.MediaBlocks.Parsers;
+using VisioForge.Core.MediaBlocks.VideoDecoders;
+using VisioForge.Core.Types.X.VideoDecoders;
+using System.Threading;
+using System.Timers;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using VisioForge.Core.MediaBlocks.Special;
+using VisioForge.Core.MediaBlocks.VideoProcessing;
+using System.Diagnostics;
+using System.Windows.Threading;
+
+namespace media_player
+{
+    public partial class Form1 : Form
+    {
+        private MediaBlocksPipeline _pipeline;
+
+        private BasicFileSourceBlock _fileSource;
+
+        private ParseBinBlock _demux;
+
+        private H264ParseBlock _parse;
+
+        private DataSampleGrabberBlock _sampleGrabber;
+
+        private NullRendererBlock _nullRenderer;
+
+        private System.Timers.Timer _tmPosition;
+
+        private long _frameCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _tmPosition = new System.Timers.Timer(100);
+            _tmPosition.Elapsed += _tmPosition_Elapsed;
+        }
+
+        private void _tmPosition_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(async () =>
+            {
+                if (_pipeline != null)
+                {
+                    var pos = await _pipeline.Position_GetAsync();
+                    lbTimestamp.Text = pos.ToString(@"hh\:mm\:ss");
+                }
+            }));
+        }
+
+        private async void btStart_Click(object sender, EventArgs e)
+        {
+            _frameCount = 0;
+
+            if (string.IsNullOrEmpty(edFilename.Text))
+            {
+                MessageBox.Show("Please select an H264 file.");
+                return;
+            }
+
+            var filename = edFilename.Text;
+            var httpSource = filename.Contains("http://") || filename.Contains("https://");
+
+            // Create MediaBlocks pipeline
+            _pipeline = new MediaBlocksPipeline();
+            _pipeline.OnStop += (sender2, args) =>
+            {
+                Invoke(new Action(() =>
+                {
+                    _tmPosition.Stop();
+
+                    MessageBox.Show("Playback complete.");
+                }));
+            };
+
+            _pipeline.Debug_Mode = true;
+            _pipeline.Debug_Dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VisioForge");
+
+            // Create file source
+            _fileSource = new BasicFileSourceBlock(edFilename.Text);
+
+            // Create demux
+            _demux = new ParseBinBlock();
+
+            // Add decoder and parser
+            _parse = new H264ParseBlock();
+
+            // Add sample grabber
+            _sampleGrabber = new DataSampleGrabberBlock(MediaBlockPadMediaType.Video);
+            _sampleGrabber.OnDataFrame += (sender2, args) =>
+            {
+                Invoke(new Action(() =>
+                {
+                    _frameCount++;
+                    lbStatus.Text = $"Received frames: {_frameCount}";
+                }));
+            };
+
+            // Add null renderer
+            _nullRenderer = new NullRendererBlock(MediaBlockPadMediaType.Video);
+            _nullRenderer.IsSync = false; // disable sync to play as fast as possible
+
+            // Connect blocks
+            _pipeline.Connect(_fileSource, _demux);
+            _pipeline.Connect(_demux.GetOutputPadByType(MediaBlockPadMediaType.Video), _parse.Input);
+            _pipeline.Connect(_parse, _sampleGrabber);
+            _pipeline.Connect(_sampleGrabber, _nullRenderer);
+
+            // Create pipeline and configure elements
+            await _pipeline.StartAsync();     
+
+            _tmPosition.Start();
+        }
+
+        private async void btStop_Click(object sender, EventArgs e)
+        {
+            _tmPosition.Stop();
+
+            await _pipeline.StopAsync();
+
+            await _pipeline.DisposeAsync();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await VisioForgeX.InitSDKAsync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            VisioForgeX.DestroySDK();
+        }
+
+        private void btOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                edFilename.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
