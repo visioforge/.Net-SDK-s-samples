eccb46dda971f47edcb2b1e0ea580abb6420786e
Roman Miniailov <support@visioforge.com>
2025-01-22 15:52:14 +0100

    Added Simple Media Player WPF sample to Media Player SDK

---
+++ Media Player SDK/WPF/CSharp/Simple Player Demo/MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using VisioForge.Core;
+using VisioForge.Core.MediaInfo;
+using VisioForge.Core.MediaPlayer;
+using VisioForge.Core.Types.Events;
+
+namespace Simple_Player_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Timers.Timer _timer;
+
+        private volatile bool _timerFlag;
+
+        private MediaPlayerCore _player;
+
+        private volatile bool _isClosing;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timer = new System.Timers.Timer(500);
+            _timer.Elapsed += _timer_Elapsed;
+
+            Title += $" (SDK v{VisioForge.Core.MediaPlayerX.MediaPlayerCoreX.SDK_Version})";
+
+            await VisioForgeX.InitSDKAsync();
+
+            CreateEngine();
+
+            foreach (var device in _player.Audio_OutputDevices())
+            {
+                cbAudioOutput.Items.Add(device);
+            }
+
+            if (cbAudioOutput.Items.Count > 0)
+            {
+                cbAudioOutput.SelectedIndex = 0;
+            }
+        }
+
+        private void Player_OnError(object sender, ErrorsEventArgs e)
+        {
+            Dispatcher.Invoke((Action)(() =>
+            {
+                edLog.Text = edLog.Text + e.Message + Environment.NewLine;
+            }));
+        }
+
+        private void Player_OnStop(object sender, StopEventArgs e)
+        {
+            if (_isClosing)
+            {
+                return;
+            }
+
+            Dispatcher.Invoke((Action)(() =>
+            {
+                tbTimeline.Value = 0;
+            }));
+        }
+
+        private void CreateEngine()
+        {
+            _player = new MediaPlayerCore(VideoView1);
+            _player.OnError += Player_OnError;
+            _player.OnStop += Player_OnStop;
+
+            _player.Debug_Dir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VisioForge");
+            _player.Debug_Mode = cbDebugMode.IsChecked == true;
+        }
+
+        private async void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _timerFlag = true;
+
+            if (_player == null)
+            {
+                return;
+            }
+
+            var position = await _player.Position_Get_TimeAsync();
+            var duration = await _player.Duration_TimeAsync();
+
+            Dispatcher.Invoke((Action)(() =>
+            {
+                tbTimeline.Maximum = (int)duration.TotalSeconds;
+
+                lbTime.Text = position.ToString("hh\\:mm\\:ss") + " | " + duration.ToString("hh\\:mm\\:ss");
+
+                if (tbTimeline.Maximum >= position.TotalSeconds)
+                {
+                    tbTimeline.Value = (int)position.TotalSeconds;
+                }
+            }));
+
+            _timerFlag = false;
+        }
+
+        private async Task DestroyEngineAsync()
+        {
+            if (_player != null)
+            {
+                _player.OnError -= Player_OnError;
+                _player.OnStop -= Player_OnStop;
+                Thread.Sleep(500);
+                await _player.DisposeAsync();
+                _player = null;
+            }
+        }
+
+        private void btSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                edFilename.Text = dialog.FileName;
+            }
+        }
+
+        private async void tbTimeline_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!_timerFlag && _player != null)
+            {
+                await _player.Position_Set_TimeAsync(TimeSpan.FromSeconds(tbTimeline.Value));
+            }
+        }
+
+        private async void btStart_Click(object sender, RoutedEventArgs e)
+        {
+            edLog.Clear();
+
+            await DestroyEngineAsync();
+
+            CreateEngine();
+
+            _player.Audio_OutputDevice = cbAudioOutput.Text;
+
+            _player.Playlist_Clear();
+            _player.Playlist_Add(edFilename.Text);
+
+            await _player.PlayAsync();
+
+            _timer.Start();
+        }
+
+        private async void btStop_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+
+            if (_player != null)
+            {
+                await _player.StopAsync();
+                await DestroyEngineAsync();
+                _player = null;
+            }
+
+            tbTimeline.Value = 0;
+        }
+
+        private async void btPause_Click(object sender, RoutedEventArgs e)
+        {
+            await _player.PauseAsync();
+        }
+
+        private async void btResume_Click(object sender, RoutedEventArgs e)
+        {
+            await _player.ResumeAsync();
+        }
+
+        private void tbVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (_player != null)
+            {
+                _player.Audio_OutputDevice_Volume_Set(0, (int)tbVolume.Value);
+            }
+        }
+
+        private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _timer.Stop();
+
+            _isClosing = true;
+
+            await DestroyEngineAsync();
+        }
+    }
+}
