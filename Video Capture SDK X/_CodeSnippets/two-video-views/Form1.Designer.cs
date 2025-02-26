f56663f57aed51aa615a35677c03559e79cf49fc
Roman Miniailov <support@visioforge.com>
2025-02-26 18:46:30 +0100

    Update to SDK version 2025.2.20

---
+++ Video Capture SDK X/_CodeSnippets/two-video-views/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace two_video_views
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            videoCapture1?.Dispose();
+            videoCapture1 = null;
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            VideoView1 = new VisioForge.Core.UI.WinForms.VideoView();
+            btStart = new System.Windows.Forms.Button();
+            btStop = new System.Windows.Forms.Button();
+            videoView2 = new VisioForge.Core.UI.WinForms.VideoView();
+            SuspendLayout();
+            // 
+            // VideoView1
+            // 
+            VideoView1.BackColor = System.Drawing.Color.Black;
+            VideoView1.Location = new System.Drawing.Point(27, 27);
+            VideoView1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            VideoView1.Name = "VideoView1";
+            VideoView1.Size = new System.Drawing.Size(517, 399);
+            VideoView1.TabIndex = 0;
+            // 
+            // btStart
+            // 
+            btStart.Location = new System.Drawing.Point(27, 438);
+            btStart.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            btStart.Name = "btStart";
+            btStart.Size = new System.Drawing.Size(125, 44);
+            btStart.TabIndex = 1;
+            btStart.Text = "Start";
+            btStart.UseVisualStyleBackColor = true;
+            btStart.Click += btStart_Click;
+            // 
+            // btStop
+            // 
+            btStop.Location = new System.Drawing.Point(162, 438);
+            btStop.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            btStop.Name = "btStop";
+            btStop.Size = new System.Drawing.Size(125, 44);
+            btStop.TabIndex = 2;
+            btStop.Text = "Stop";
+            btStop.UseVisualStyleBackColor = true;
+            btStop.Click += btStop_Click;
+            // 
+            // videoView2
+            // 
+            videoView2.BackColor = System.Drawing.Color.Black;
+            videoView2.Location = new System.Drawing.Point(554, 27);
+            videoView2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            videoView2.Name = "videoView2";
+            videoView2.Size = new System.Drawing.Size(517, 399);
+            videoView2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1123, 503);
+            Controls.Add(videoView2);
+            Controls.Add(btStop);
+            Controls.Add(btStart);
+            Controls.Add(VideoView1);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            Name = "Form1";
+            Text = "Video Capture SDK .Net - Two Video Views";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private VisioForge.Core.UI.WinForms.VideoView VideoView1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private VisioForge.Core.UI.WinForms.VideoView videoView2;
+    }
+}
---
+++ Video Capture SDK X/_CodeSnippets/two-video-views/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using VisioForge.Core.VideoCapture;
+using VisioForge.Core.Types;
+using VisioForge.Core.VideoCaptureX;
+using VisioForge.Core.Types.X.Sources;
+using VisioForge.Core.Types.X.Output;
+using VisioForge.Core;
+using System.Linq;
+
+namespace two_video_views
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureCoreX videoCapture1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private IScreenCaptureSourceSettings CreateScreenCaptureSource()
+        {
+            var source = new ScreenCaptureDX9SourceSettings();
+
+            source.FrameRate = new VideoFrameRate(25);
+            source.Rectangle = new VisioForge.Core.Types.Rect(System.Windows.Forms.Screen.AllScreens[0].Bounds);
+
+            source.CaptureCursor = true;
+            source.Monitor = 0;
+
+            return source;
+        }
+
+        private async void btStart_Click(object sender, EventArgs e)
+        {
+            videoCapture1 = new VideoCaptureCoreX(VideoView1);
+            videoCapture1.Video_Views.Add(videoView2);
+
+            // set screen capture with full screen enabled
+            videoCapture1.Video_Source = CreateScreenCaptureSource();
+
+            // disable audio playback and recording
+            videoCapture1.Audio_Play = videoCapture1.Audio_Record = false;
+
+            await videoCapture1.StartAsync();
+        }
+
+        private async void btStop_Click(object sender, EventArgs e)
+        {
+            await videoCapture1.StopAsync();
+
+            await videoCapture1.DisposeAsync();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await VisioForgeX.InitSDKAsync();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            VisioForgeX.DestroySDK();
+        }
+    }
+}
