ee829bb16c9ed8ca35597fc8281a80c0f14c6d4f
Roman Miniailov <support@visioforge.com>
2025-03-10 12:37:22 +0100

    Update to SDK version 2025.3.7

--- Media Player SDK/WinForms/VB .Net/Two Windows/My Project/AssemblyInfo.vb
+++ Media Player SDK/WinForms/VB .Net/Two Windows/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2025.3.6.0")>
-<Assembly: AssemblyFileVersion("2025.3.6.0")>
+<Assembly: AssemblyVersion("2025.3.7.0")>
+<Assembly: AssemblyFileVersion("2025.3.7.0")>
