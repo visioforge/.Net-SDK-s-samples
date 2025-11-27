440e215e43d694c9b4daaf4e70b01a6dabafb968
Roman Miniailov <support@visioforge.com>
2025-11-27 09:52:56 +0100

    Update to SDK version 2025.11.7

--- Video Capture SDK/WinForms/VB.Net/DV Capture/My Project/AssemblyInfo.vb
+++ Video Capture SDK/WinForms/VB.Net/DV Capture/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2025.11.6.0")>
-<Assembly: AssemblyFileVersion("2025.11.6.0")>
+<Assembly: AssemblyVersion("2025.11.7.0")>
+<Assembly: AssemblyFileVersion("2025.11.7.0")>
